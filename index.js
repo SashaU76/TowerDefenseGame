6a83c3ae5eddcc640ddf008e16bccd148c8a3b51
SashaU76 <zlojbass@rambler.ru>
2021-02-27 00:40:38 +0300

    size2

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-canvas.width=1500;
-canvas.height=940;
+canvas.width=window.innerWidth-10;
+canvas.height=window.innerHeight;
 
 
 //global variables
